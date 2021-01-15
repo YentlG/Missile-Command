1e24a1903e583405a14e250ec430a4a8d6fa8899
Yentl Gevers <yentl.gevers@leerling.piustien.net>
2021-01-15 11:52:18 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,56 +1,90 @@
 let player = null;
-let playerSpeed = 5;
+let playerSpeed = 10;
+let explosions = [];
+let explosionLife = 100;
+let shootTimer = 0;
+let shotsPerSecond = 4;
 
 function setup() {
-    createCanvas(800, 500);
+    createCanvas(400, 400);
 
     player = createSprite(width / 2, height - 20, 20, 20);
-    player.draw = TekenSpeler;
-    
+    player.draw = DrawPlayer;
 }
+
 
 function draw() {
     background(0, 0, 0);
 
-    BeweegSpeler();
+    MovePlayer();
+
+    Shoot();
+    RemoveDeadExplosions();
 
     drawSprites();
 }
 
-function TekenSpeler() {
-    fill(0);
-    stroke(255);
-    strokeWeight(2);
+function RemoveDeadExplosions() {
+    if (explosions.length > 0 && explosions[0].life == 0) {
+        explosions.shift();
+    }
+}
 
-    circle(0, 0, this.width);   
+
+function Shoot() {
+    shootTimer += deltaTime;
+    if (keyIsDown(32) && shootTimer >= 1000 / shotsPerSecond) {
+        CreateExplosion(player.position.x, player.position.y);
+        shootTimer = 0;
+    }
+
+    function CreateExplosion(x, y) {
+        let explosion = createSprite(x, y, 5, 5);
+        explosion.life = 100;
+        explosion.draw = DrawExplosion;
+        explosions.push(explosion);
+    }
+
+    function DrawExplosion() {
+        circle(0, 0, this.width);
+        this.width++;
+        this.height++;
+    }
+
+    function DrawPlayer() {
+        fill(0);
+        stroke(255);
+        strokeWeight(2);
+
+        circle(0, 0, this.width);
     
-    line(0, 5, 0, 20);
-    line(0, -5, 0, -20);
-    line(5, 0, 20, 0);
-    line(-5, 0, -20, 0);
-    /*
-    line(0, 0, 0, 50);
-    line(0, 50, -10, 70);
-    line(0, 50, 10, 70);
+        line(0, 5, 0, 20);
+        line(0, -5, 0, -20);
+        line(5, 0, 20, 0);
+        line(-5, 0, -20, 0);
+        /*
+        line(0, 0, 0, 50);
+        line(0, 50, -10, 70);
+        line(0, 50, 10, 70);
+    
+        line(0, 25, -20, 10);
+        line(0, 25, 20, 70);
+    
+        rect(25, 50, 30, 20);
+        */
+    }
 
-    line(0, 25, -20, 10);
-    line(0, 25, 20, 70);
-
-    rect(25, 50, 30, 20);
-    */
-}
-
-function BeweegSpeler() {
-    if (keyIsDown(DOWN_ARROW)) {
-        player.position.y += 6;
+    function MovePlayer() {}
+        if (keyIsDown(DOWN_ARROW)) {
+            player.position.y += 6;
+        }
+        if (keyIsDown(UP_ARROW)) {
+            player.position.y -= 6;
+        }
+        if (keyIsDown(LEFT_ARROW)) {
+            player.position.x -= 6;
+        }
+        if (keyIsDown(RIGHT_ARROW)) {
+            player.position.x += 6;
+        }
     }
-    if (keyIsDown(UP_ARROW)) {
-        player.position.y -= 6;
-    }
-    if (keyIsDown(LEFT_ARROW)) {
-        player.position.x -= 6;
-    }
-    if (keyIsDown(RIGHT_ARROW)) {
-        player.position.x += 6;
-    }
-}
