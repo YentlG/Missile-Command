a9d7fcaf5a75d57ec703a01c5505c72c859fef5f
Yentl Gevers <yentl.gevers@leerling.piustien.net>
2021-02-04 13:46:02 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,44 @@
 let player = null;
-let playerSpeed = 10;
+let gun = null;
+let enemyMissilesGroup = null;
 
-let explosions = [];
+// how fast the player can move
+let playerSpeed = 10;
+// how many milliseconds since the players' last shot
+let shootTimer = 0;
+// how many shots a player can fire per second
+let shotsPerSecond = 4;
+// how fast the players' missiles will fly
+let missileSpeed = 5;
+
+
+// how many milliseconds since the enemy's last shot
+let enemyShootTimer = 0;
+// how many shots the enemy can fire per second
+let enemyShotsPerSecond = 1;
+// how many shots the enemy can fire per second
+let maxEnemyMissiles = 3;
+// how fast the enemy's missiles will fly
+let enemyMissileSpeed = 2;
+
+// how many frames an explosion will last
 let explosionLife = 100;
 
-let shootTimer = 0;
-let shotsPerSecond = 4;
-
-let enemyShootTimer = 0;
-let enemyShotsPerSecond = 1;
-
-let friendlyMissiles = [];
-let gun = null;
-
-let enemyMissilesGroup
 
 function setup() {
-    createCanvas(800, 500);
     angleMode(DEGREES);
+    createCanvas(800, 500);
 
     player = createSprite(width / 2, height - 100, 20, 20);
     player.draw = DrawPlayer;
 
-    gun = createSprite(width / 2, height - 40, 20, 20);
+    gun = createSprite(width / 2, height - 50, 25, 25);
 
-
+    enemyMissilesGroup = new Group();
 }
 
 function draw() {
-    background(0, 0, 0);
+    background(0);
 
     MovePlayer();
     Shoot();
@@ -38,84 +48,74 @@ function draw() {
     drawSprites();
 }
 
-function EnemyShootsMissile() {
+function EnemyShootsMissile() { 
     enemyShootTimer += deltaTime;
-    if (keyIsDown(32) && shootTimer >= 1000 / shotsPerSecond) {
-        CreateFriendlyMissiles();
-        shootTimer = 0;
+    if (enemyMissilesGroup.length < maxEnemyMissiles &&
+        enemyShootTimer >= 1000 / enemyShotsPerSecond) {
+        CreateEnemyMissile();
+        enemyShootTimer = 0;
     }
 }
 
-function CreateFriendlyMissiles() {
+function CreateFriendlyMissile() { 
     let startPosition = gun.position.copy();
     let endPosition = player.position.copy();
-    let direction = p5.vector.sub(endPosition, startPosition);
+    let direction = p5.Vector.sub(endPosition, startPosition);
 
-    let missile = createSprite(start.x, start.y, 5, 5);
-    missile.setSpeed(2, directionAngle);
+    let missile = createSprite(startPosition.x, startPosition.y, 5, 5);
+    missile.setSpeed(missileSpeed, direction.heading());
     missile["goal"] = endPosition;
-    missile.draw = DrawFriendlyMissiles;
+
+    missile.draw = DrawFriendlyMissile;
 }
 
-function DrawFriendlyMissiles() {
+function DrawFriendlyMissile() { 
     circle(0, 0, this.width);
 
-    let currentPosition = this.position;
-    let goalPosition = this.goal;
-    let distance = p5.vector.dist(this.position, this.goal);
-
-    if (distance < 5) {
-        CreateExplosion(currentPosition.x, currentPosition.y);
-        this.remove;
+    let distance = p5.Vector.dist(this.position, this.goal);
+    if (distance < 5) { 
+        this.remove();
+        CreateExplosion(this.goal.x, this.goal.y);
     }
-} 
+}
 
-function CreateEnemyMissiles() {
+function CreateEnemyMissile() { 
     let startX = random(0, width);
-    let endPosition = createVector(endX, 0);
     let startPosition = createVector(startX, 0);
     let endX = random(0, width);
-    let direction = p5.vector.sub(endPosition, startPosition);
+    let endPosition = createVector(endX, height);
+    let direction = p5.Vector.sub(endPosition, startPosition);
 
-    let missile = createSprite(start.x, start.y, 5, 5);
-    missile.setSpeed(5, directionAngle);
+    let missile = createSprite(startPosition.x, startPosition.y, 5, 5);
+    missile.setSpeed(enemyMissileSpeed, direction.heading());
     missile["goal"] = endPosition;
 
     enemyMissilesGroup.add(missile);
 
-    missile.draw = DrawEnemyMissiles;
+    missile.draw = DrawEnemyMissile;
 }
 
-function DrawEnemyMissiles() {
+function DrawEnemyMissile() { 
+    fill(255, 0, 0);
     circle(0, 0, this.width);
 
-    let currentPosition = this.position;
-    let goalPosition = this.goal;
-    let distance = p5.vector.dist(this.position, this.goal);
-
-    if (distance < 5) {
-        CreateExplosion(currentPosition.x, currentPosition.y);
-        this.remove;
-    }
-} 
-    
-function RemoveDeadExplosions() {
-    if (explosions.length > 0 && explosions[0].life == 0) {
-        explosions.shift();
+    let distance = p5.Vector.dist(this.position, this.goal);
+    if (distance < 5) { 
+        this.remove();
+        CreateExplosion(this.goal.x, this.goal.y);
     }
 }
-
 
 function Shoot() {
     shootTimer += deltaTime;
     if (keyIsDown(32) && shootTimer >= 1000 / shotsPerSecond) {
-        CreateFriendlyMissiles();
+        CreateFriendlyMissile();
         shootTimer = 0;
     }
 }
 
 function CreateExplosion(x, y) {
-    let explosion = createSprite(x, y, 5, 5);
+    let explosion = createSprite(x, y, 1, 1);
     explosion.life = explosionLife;
     explosion.draw = DrawExplosion;
 }
@@ -125,10 +125,10 @@ function DrawExplosion() {
     this.width++;
     this.height++;
 
-    this.overlap(enemyMissilesGroup, enemyMissileIsHit);
+    this.overlap(enemyMissilesGroup, EnemyMissileIsHit);
 }
 
-function EnemyMissileIsHit(explosion, enemyMissile) {
+function EnemyMissileIsHit(explosion, enemyMissile) { 
     enemyMissile.remove();
 }
 
@@ -136,7 +136,6 @@ function DrawPlayer() {
     fill(0);
     stroke(255);
     strokeWeight(2);
-
     circle(0, 0, this.width);
 
     line(0, 5, 0, 20);
@@ -147,15 +146,16 @@ function DrawPlayer() {
 
 function MovePlayer() {
     if (keyIsDown(DOWN_ARROW)) {
-        player.position.y += 6;
+        player.position.y += playerSpeed;
     }
     if (keyIsDown(UP_ARROW)) {
-        player.position.y -= 6;
+        player.position.y -= playerSpeed;
     }
+
     if (keyIsDown(LEFT_ARROW)) {
-        player.position.x -= 6;
+        player.position.x -= playerSpeed;
     }
     if (keyIsDown(RIGHT_ARROW)) {
-        player.position.x += 6;
+        player.position.x += playerSpeed;
     }
 }
